9754782e5545b97b37481f5d16b74278cd6672d1
Anton Lebedev <wrawka@gmail.com>
2023-12-04 23:57:39 +0700

    Day3 is bullshit :(((

---
+++ Day3/solution.py
@@ -0,0 +1,22 @@
+from io import TextIOWrapper
+from pathlib import Path
+
+
+class StupidSchematicParser:
+    """"""
+
+    def __init__(self, schematic: TextIOWrapper) -> None:
+        self.scematic = schematic
+
+    def _
+
+if __name__ == '__main__':
+    TEST_INPUT_PATH = Path(__file__).resolve().parent.joinpath('test_input.txt')
+    TEST_ANSWER = 4361
+    INPUT_PATH = Path(__file__).resolve().parent.joinpath('input.txt')
+
+    with open(INPUT_PATH) as input_file:
+        pass
+
+    # assert sum_of_ids == TEST_IDS_SUM, f'{sum_of_ids} is not {TEST_IDS_SUM}'
+    # assert sum_of_powers == TEST_POWERS_SUM, f'{sum_of_powers} is not {TEST_POWERS_SUM}'
